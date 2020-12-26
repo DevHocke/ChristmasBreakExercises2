e14ce7380268f357f75056035b71de04d4a8a68c
Håkan Eriksson <badfish.paint@gmail.com>
2020-12-26 20:03:55 +0100

    Exercise 1 completed, Started doing 1-3 and then added 4. Thats why i did not save as array directly.

--- ChristmasBreakExercises2/Program.cs
+++ ChristmasBreakExercises2/Program.cs
@@ -4,9 +4,34 @@ namespace ChristmasBreakExercises2
 {
     class Program
     {
+
+        //1. Skapa ett program som ber användaren skriva in 3 nummer
+        //2. Addera talen
+        //3. Skriv ut summan
+        //4. Skriv sedan ut dem i storleks ordning.Störst först
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Write a number: ");
+            int one = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Write a second number: ");
+            int two = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Write a third number: ");
+            int three = Convert.ToInt32(Console.ReadLine());
+           
+            int sum = one + two + three;
+
+            Console.WriteLine($"Summan av talen är {sum}");
+            int[] numbers = new int[3] { one, two, three };
+
+            Array.Sort(numbers);
+            Array.Reverse(numbers);
+
+            foreach (var item in numbers)
+            {
+                Console.WriteLine(item);
+            }
+            
         }
+        
     }
 }
